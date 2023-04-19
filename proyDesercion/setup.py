25b17850e131446903891d7d5302ff4ebaf9ffab
Vfuentealba <veronica.fuentealba.riquelme@gmail.com>
2023-04-19 16:01:10 -0400

    Iniciando Proyecto

---
+++ proyDesercion/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Análisis y predicción de alumnos dersertores de primer año',
+    author='GST',
+    license='',
+)
